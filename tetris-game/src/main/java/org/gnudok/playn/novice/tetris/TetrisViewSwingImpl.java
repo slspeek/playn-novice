d1feef6b2a44416c24b37ec71e87d3bd4dde90bb
Jan Terheggen <janterheggen@gmail.com>
2011-10-31 09:46:05 -0400

    Overriding of paint more exact.

--- tetris-game/src/main/java/org/gnudok/playn/novice/tetris/TetrisViewSwingImpl.java
+++ tetris-game/src/main/java/org/gnudok/playn/novice/tetris/TetrisViewSwingImpl.java
@@ -1,12 +1,16 @@
 package org.gnudok.playn.novice.tetris;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.KeyListener;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+@SuppressWarnings("javadoc")
 public class TetrisViewSwingImpl extends JPanel implements TetrisView {
 
 	/**
@@ -14,10 +18,28 @@ public class TetrisViewSwingImpl extends JPanel implements TetrisView {
 	 */
 	private static final long serialVersionUID = -110735668571632303L;
 	Paintable p;
+	JPanel innerBoard  = new JPanel() {
+		public void paint(Graphics g) {
+			super.paint(g);
+			TetrisGraphics tg = new TetrisGraphicsAWTImpl(g);
+			p.onPaint(tg);
+		}
+	};
+	JLabel status = new JLabel("Kikeboe");
+	
+	public TetrisViewSwingImpl() {
+		setBackground(Color.CYAN);
+		setForeground(Color.yellow);
+		this.status.setBackground(Color.blue);
+		this.status.setSize(100,100);
+		setLayout(new BorderLayout());
+		add(this.innerBoard, BorderLayout.CENTER);
+		add(this.status, BorderLayout.SOUTH);
+	}
+	
 	@Override
 	public void setStatusText(String text) {
-		
-
+		status.setText(text);
 	}
 
 	
@@ -29,7 +51,8 @@ public class TetrisViewSwingImpl extends JPanel implements TetrisView {
 
 	@Override
 	public Dimension getSize() {
-		return super.getSize();
+		Dimension s = innerBoard.getSize();
+		return new Dimension(s.width, s.height - 20);
 	}
 
 	@Override
@@ -46,14 +69,9 @@ public class TetrisViewSwingImpl extends JPanel implements TetrisView {
 	@Override
 	public void setPaintable(Paintable p) {
 		this.p = p;
-		
 	}
 	
-	public void paint(Graphics g) {
-		super.repaint();
-		TetrisGraphics tg = new TetrisGraphicsAWTImpl(g);
-		p.onPaint(tg);
-	}
+	
 	
 
 }
