5e08f11b6271f3fa06dbb157e060f6c1612c8253
Steven Speek <slspeek@gmail.com>
2011-11-02 16:27:05 +0100

    20 Fps.

--- tetris/core/src/main/java/org/gnudok/playn/novice/tetris/core/Tetris.java
+++ tetris/core/src/main/java/org/gnudok/playn/novice/tetris/core/Tetris.java
@@ -6,34 +6,37 @@ import playn.core.CanvasLayer;
 import playn.core.Game;
 
 public class Tetris implements Game {
-	
+
 	Board board;
 	Canvas canvas;
-	
-  @Override
-  public void init() {
-    CanvasLayer canvasLayer = graphics().createCanvasLayer(200, 400);
-    canvasLayer.setTranslation(50,50);
-    canvas = canvasLayer.canvas();
-    board = new Board(canvas);
-    board.start();
-    graphics().rootLayer().add(canvasLayer);
-    
-    
-  }
+	int updateCount = 0;
 
-  @Override
-  public void paint(float alpha) {
-	  board.onPaint(canvas);
-  }
+	@Override
+	public void init() {
+		CanvasLayer canvasLayer = graphics().createCanvasLayer(200, 400);
+		canvasLayer.setTranslation(50, 50);
+		canvas = canvasLayer.canvas();
+		board = new Board(canvas);
+		board.start();
+		graphics().rootLayer().add(canvasLayer);
 
-  @Override
-  public void update(float delta) {
-	  board.update();
-  }
+	}
 
-  @Override
-  public int updateRate() {
-    return 500;
-  }
+	@Override
+	public void paint(float alpha) {
+		board.onPaint(canvas);
+	}
+
+	@Override
+	public void update(float delta) {
+		updateCount++;
+		if (updateCount % 10 == 0) {
+			board.update();
+		}
+	}
+
+	@Override
+	public int updateRate() {
+		return 50;
+	}
 }
