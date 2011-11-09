60d457606e100fe36f5dcc8494fe774af901c223
Jan Terheggen <janterheggen@gmail.com>
2011-11-09 16:31:51 +0100

    Skeletion for clean code example

---
+++ clean-code-polymorphy/src/main/java/org/gnudok/playn/novice/clean/Node.java
@@ -0,0 +1,11 @@
+package org.gnudok.playn.novice.clean;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author jan.
+ *         Created 9 nov. 2011.
+ */
+public class Node {
+
+}
