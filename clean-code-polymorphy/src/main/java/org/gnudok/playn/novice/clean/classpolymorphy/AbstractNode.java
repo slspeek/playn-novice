aa8171a1653a40ad711f645665b70cbcc4cc05d9
Youssef <youssefouladfaris@gmail.com>
2011-11-11 14:29:29 +0100

    Added quotient and AbstractNode

---
+++ clean-code-polymorphy/src/main/java/org/gnudok/playn/novice/clean/classpolymorphy/AbstractNode.java
@@ -0,0 +1,14 @@
+package org.gnudok.playn.novice.clean.classpolymorphy;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author jan.
+ *         Created 9 nov. 2011.
+ */
+public abstract class AbstractNode {
+	
+	
+	public abstract double evaluate();
+	
+}
