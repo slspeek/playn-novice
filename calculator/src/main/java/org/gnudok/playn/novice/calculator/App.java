d6bf425ba0fe91097bd768772b42f9cb448676bb
Youssef <youssefouladfaris@gmail.com>
2011-11-22 13:08:30 +0100

    added calculator skeleton

---
+++ calculator/src/main/java/org/gnudok/playn/novice/calculator/App.java
@@ -0,0 +1,13 @@
+package org.gnudok.playn.novice.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
