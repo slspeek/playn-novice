555c2ff517da60c742be7087e1c2089e38815100
Youssef <youssefouladfaris@gmail.com>
2011-11-22 13:05:57 +0100

    added dayoflife skeleton

---
+++ dayoflife/src/main/java/org/gnudok/playn/novice/dayofpackage/App.java
@@ -0,0 +1,13 @@
+package org.gnudok.playn.novice.dayofpackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
