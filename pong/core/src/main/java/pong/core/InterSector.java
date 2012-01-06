1f71e2d048f51f6fbbd273e4e38db92a4a572db9
Steven Speek <slspeek@gmail.com>
2012-01-06 13:51:14 +0100

    Collistion object with time added.

--- pong/core/src/main/java/pong/core/InterSector.java
+++ pong/core/src/main/java/pong/core/InterSector.java
@@ -3,9 +3,11 @@ package pong.core;
 import org.jbox2d.common.Vec2;
 
 public class InterSector {
-	Vec2 getCollision(Vec2 balPos, Vec2 balVel, float c) {
+	Collision getCollision(Vec2 balPos, Vec2 balVel, float c) {
 		float x = balPos.x +((c - balPos.y / balVel.y)* balVel.x);
 		Vec2 returnValue = new Vec2(x,c);
-		return returnValue;
+		float time = (c - balPos.y)/balVel.y;
+		Collision col = new Collision(returnValue, time);
+		return col;
 	}
 }
