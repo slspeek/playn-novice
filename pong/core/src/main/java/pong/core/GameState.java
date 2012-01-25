e86dfa3e444b55368009dde13306392d85cc5ef6
Steven Speek <slspeek@gmail.com>
2012-01-25 15:52:21 +0100

    Add enum

---
+++ pong/core/src/main/java/pong/core/GameState.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pong.core;
+
+/**
+ *
+ * @author steven
+ */
+public enum GameState {
+    
+    BeforeStart,
+    Running,
+    Paused,
+    GameOver;
+    
+}
