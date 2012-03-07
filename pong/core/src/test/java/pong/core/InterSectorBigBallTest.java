69d561d004c6a98fff4b464331ac40dfe0a26fd2
Steven Speek <slspeek@gmail.com>
2012-03-07 12:21:05 +0100

    Bigger balls can be predicted

--- pong/core/src/test/java/pong/core/InterSectorBigBallTest.java
+++ pong/core/src/test/java/pong/core/InterSectorBigBallTest.java
@@ -19,4 +19,42 @@ public class InterSectorBigBallTest {
         Collision col = interSector.getPrediction(ballPos, ballVel, 0);
         assertEquals(expectedCollision, col.getPosition());
     }
+    
+    @Test
+    public void testGetCollision2() {
+
+        interSector = new InterSector(4f, 3f, 0.5f);
+        Vec2 ballPos = new Vec2(2f, 2.5f);
+        Vec2 ballVel = new Vec2(1.5f, -.5f);
+
+        Vec2 expectedCollision = new Vec2(2f, .5f);
+        Collision col = interSector.getPrediction(ballPos, ballVel, 0);
+        assertEquals(expectedCollision, col.getPosition());
+    }
+    
+    
+    
+    @Test
+    public void testGetCollision3() {
+
+        interSector = new InterSector(4f, 3f, 0.5f);
+        Vec2 ballPos = new Vec2(1f, 2.5f);
+        Vec2 ballVel = new Vec2(1f, -2f);
+
+        Vec2 expectedCollision = new Vec2(2f, .5f);
+        Collision col = interSector.getPrediction(ballPos, ballVel, 0);
+        assertEquals(expectedCollision, col.getPosition());
+    }
+    
+    @Test(expected=IllegalStateException.class)
+    public void testGetCollision4() {
+
+        interSector = new InterSector(4f, 3f, 0.5f);
+        Vec2 ballPos = new Vec2(0.5f, 1.5f);
+        Vec2 ballVel = new Vec2(1f, 1f);
+
+        Vec2 expectedCollision = new Vec2(3f, 1f);
+        Collision col = interSector.getPrediction(ballPos, ballVel, 0);
+        assertEquals(expectedCollision, col.getPosition());
+    }
 }
