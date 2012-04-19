4f24a8ac24a04397feb058e9fd16271a9f026a37
Steven Speek <slspeek@gmail.com>
2012-04-19 14:40:37 +0200

    Html & Android with new factory too

--- pong/html/src/main/java/pong/html/PongGameHtml.java
+++ pong/html/src/main/java/pong/html/PongGameHtml.java
@@ -1,17 +1,20 @@
 package pong.html;
 
+import playn.core.Game;
 import playn.core.PlayN;
 import playn.html.HtmlGame;
 import playn.html.HtmlPlatform;
 
 import pong.core.PongGame;
+import pong.core.PongGameFactory;
 
 public class PongGameHtml extends HtmlGame {
 
-  @Override
-  public void start() {
-    HtmlPlatform platform = HtmlPlatform.register();
-    platform.assetManager().setPathPrefix("pong/");
-    PlayN.run(new PongGame());
-  }
+    @Override
+    public void start() {
+        HtmlPlatform platform = HtmlPlatform.register();
+        platform.assetManager().setPathPrefix("pong/");
+        Game game = (new PongGameFactory()).get();
+        PlayN.run(game);
+    }
 }
