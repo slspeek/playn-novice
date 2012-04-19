f7738136eb5749093e8d72c5aa1e389a542502f1
Steven Speek <slspeek@gmail.com>
2012-04-19 14:40:37 +0200

    Html & Android with new factory too

--- pong/android/src/main/java/pong/android/PongGameActivity.java
+++ pong/android/src/main/java/pong/android/PongGameActivity.java
@@ -1,15 +1,18 @@
 package pong.android;
 
 import playn.android.GameActivity;
+import playn.core.Game;
 import playn.core.PlayN;
 
 import pong.core.PongGame;
+import pong.core.PongGameFactory;
 
 public class PongGameActivity extends GameActivity {
 
-  @Override
-  public void main(){
-    platform().assetManager().setPathPrefix("pong/resources");
-    PlayN.run(new PongGame());
-  }
+    @Override
+    public void main() {
+        platform().assetManager().setPathPrefix("pong/resources");
+        Game game = (new PongGameFactory()).get();
+        PlayN.run(game);
+    }
 }
